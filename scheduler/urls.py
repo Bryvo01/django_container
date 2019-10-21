7bf2abba527fcfdd9f7c4f130ab689c416a3a9a8
Bryan LeVau <Bryvo01@users.noreply.github.com>
2019-10-21 05:59:00 -0500

    added about us - nw

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
+#from django.views.generic import TemplateView
 
 urlpatterns = [
-    # path("", include("hello.urls")),
+    path('', include("main.urls")),
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html')),
+    #path('', TemplateView.as_view(template_name='home.html')),
 ]
